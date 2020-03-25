e780d08601e1c416885023b7c9556e9d66b6a293
tripdancer0916 <tripdancer0916@gmail.com>
2020-03-25 18:52:00 +0900

    debug

--- model.py
+++ model.py
@@ -15,8 +15,6 @@ class RecurrentNeuralNetwork(nn.Module):
         self.w_hh = nn.Linear(n_hid, n_hid, bias=use_bias)
         self.w_out = nn.Linear(n_hid, n_out, bias=False)
 
-        self.alpha = nn.Linear(1, n_hid, bias=False)
-
         self.activation = activation
         self.sigma_neu = sigma_neu
         self.sigma_syn = sigma_syn

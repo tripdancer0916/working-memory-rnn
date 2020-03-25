effff5419ce0d8066b172c0a330273d8152c26c0
tripdancer0916 <tripdancer0916@gmail.com>
2020-03-25 18:48:54 +0900

    romo taskを解いてみる

---
+++ model.py
@@ -0,0 +1,67 @@
+"""define recurrent neural networks"""
+
+import torch
+import torch.nn as nn
+
+
+class RecurrentNeuralNetwork(nn.Module):
+    def __init__(self, n_in, n_out, n_hid, device,
+                 alpha_time_scale=0.25, beta_time_scale=0.1, activation='tanh', sigma_neu=0.05, sigma_syn=0.002, use_bias=True):
+        super(RecurrentNeuralNetwork, self).__init__()
+        self.n_in = n_in
+        self.n_hid = n_hid
+        self.n_out = n_out
+        self.w_in = nn.Linear(n_in, n_hid, bias=False)
+        self.w_hh = nn.Linear(n_hid, n_hid, bias=use_bias)
+        self.w_out = nn.Linear(n_hid, n_out, bias=False)
+
+        self.alpha = nn.Linear(1, n_hid, bias=False)
+
+        self.activation = activation
+        self.sigma_neu = sigma_neu
+        self.sigma_syn = sigma_syn
+        self.device = device
+
+        self.alpha = torch.ones(self.n_hid) * alpha_time_scale
+        self.beta = torch.ones(self.n_hid) * beta_time_scale
+        self.alpha = self.alpha.to(self.device)
+        self.beta = self.beta.to(self.device)
+
+    def make_neural_noise(self, hidden, alpha):
+        return torch.randn_like(hidden).to(self.device) * self.sigma_neu * torch.sqrt(alpha)
+
+    def make_synaptic_plasticity(self, firing_rate, synapse, beta):
+        return torch.ger(firing_rate, firing_rate.T) + \
+               torch.randn_like(synapse).to(self.device) * self.sigma_syn * torch.sqrt(beta)
+
+    def forward(self, input_signal, hidden):
+        num_batch = input_signal.size(0)
+        length = input_signal.size(1)
+        hidden_list = torch.zeros(length, num_batch, self.n_hid).type_as(input_signal.data)
+        output_list = torch.zeros(length, num_batch, self.n_out).type_as(input_signal.data)
+
+        input_signal = input_signal.permute(1, 0, 2)
+
+        different_j = torch.zeros((num_batch, self.n_hid, self.n_hid)).to(self.device)
+        additional_w = torch.zeros((num_batch, self.n_hid, self.n_hid)).to(self.device)
+        for t in range(length):
+            activated = torch.tanh(hidden)
+
+            different_j_activity = torch.matmul(activated.unsqueeze(1), different_j).squeeze(1)
+            tmp_hidden = self.w_in(input_signal[t]) + self.w_hh(activated) + different_j_activity
+
+            neural_noise = self.make_neural_noise(hidden, self.alpha)
+            hidden = (1 - self.alpha) * hidden + self.alpha * tmp_hidden + neural_noise
+
+            additional_w = self.make_synaptic_plasticity(activated, additional_w, self.alpha)
+            new_j = (1 - self.beta) * self.w_hh.weight + self.beta * additional_w
+            different_j = new_j - self.w_hh.weight
+
+            output = self.w_out(hidden)
+            hidden_list[t] = hidden
+            output_list[t] = output
+
+        hidden_list = hidden_list.permute(1, 0, 2)
+        output_list = output_list.permute(1, 0, 2)
+
+        return hidden_list, output_list, hidden
---
+++ romo_dataset.py
@@ -0,0 +1,39 @@
+"""generating input and target"""
+
+import numpy as np
+import torch.utils.data as data
+
+
+class RomoDataset(data.Dataset):
+    def __init__(self, time_length, freq_min, freq_max, min_interval, signal_length):
+        self.time_length = time_length
+        self.freq_min = freq_min
+        self.freq_max = freq_max
+        self.min_interval = min_interval
+        self.signal_length = signal_length
+
+    def __len__(self):
+        return 200
+
+    def __getitem__(self, item):
+        # input signal
+        signal = np.zeros(self.time_length + 1)
+        first_signal_timing = np.random.randint(self.time_length - self.signal_length - 5)
+        second_signal_timing = np.random.randint(first_signal_timing, self.time_length - self.signal_length)
+
+        first_signal_freq = np.random.rand() * (self.freq_max - self.freq_min) + self.freq_min
+        while True:
+            second_signal_freq = np.random.rand() * (self.freq_max - self.freq_min) + self.freq_min
+            if abs(second_signal_freq - first_signal_freq) > self.min_interval:
+                break
+        t = np.arange(0, self.signal_length)
+        signal[first_signal_timing: first_signal_timing+self.signal_length] = np.sin(first_signal_freq * t)
+        signal[second_signal_timing:second_signal_timing+self.signal_length] = np.sin(second_signal_freq * t)
+
+        # target
+        if first_signal_freq > second_signal_freq:
+            target = np.array([1, 0])
+        else:
+            target = np.array([0, 1])
+
+        return signal, target

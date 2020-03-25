d813d784554e1d90d7372a9f0b78eaeb5ec01b4a
tripdancer0916 <tripdancer0916@gmail.com>
2020-03-25 19:19:24 +0900

    crossentropylossパイセン！！

--- train_romo.py
+++ train_romo.py
@@ -79,10 +79,11 @@ def main(config_path):
             loss.backward()
             optimizer.step()
 
-        if epoch > 0 and epoch % cfg['TRAIN']['NUM_SAVE_EPOCH'] == 0:
+        if epoch > 0 and epoch % 10 == 0:
             print(f'Train Epoch: {epoch}, Loss: {loss.item():.6f}')
             print('output', output[0, :, 0].cpu().detach().numpy())
             print('target', target[0, :, 0].cpu().detach().numpy())
+            if epoch > 0 and epoch % cfg['TRAIN']['NUM_SAVE_EPOCH'] == 0:
             torch.save(model.state_dict(), os.path.join(save_path, f'epoch_{epoch}.pth'))
 
 

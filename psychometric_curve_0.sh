2678ab730d35a65077848077b56f2093213c5bb3
tripdancer0916 <tripdancer0916@gmail.com>
2020-04-03 12:36:44 +0900

    sigma_in=0で実験

---
+++ psychometric_curve_0.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+for i in {0..21}; do
+    for((j=`expr ${i} \* 2 + 1`;j<=`expr \( ${i} + 1 \) \* 2`;j++)); do
+        echo ${j}
+        nohup python -u psychometric_curve.py cfg/romo_config/${j}.cfg --sigma_in 0 > log/psychometric_curve/${j}_0.log &
+    done
+
+    # 終了待ち
+    wait
+
+done
+
+

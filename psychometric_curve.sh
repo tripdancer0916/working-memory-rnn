cb70b7aa9d0d90a5e7641b30de8dbecee5eb5125
tripdancer0916 <tripdancer0916@gmail.com>
2020-04-03 09:30:09 +0900

    もう一回計測し直しかなあ

--- psychometric_curve.sh
+++ psychometric_curve.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in {0..19}; do
+for i in {0..21}; do
     for((j=`expr ${i} \* 2 + 1`;j<=`expr \( ${i} + 1 \) \* 2`;j++)); do
         echo ${j}
         nohup python -u psychometric_curve.py cfg/romo_config/${j}.cfg > log/psychometric_curve/${j}.log &

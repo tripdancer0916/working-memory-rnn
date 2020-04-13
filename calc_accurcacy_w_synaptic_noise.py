c3b6cf69d77b9a8f5697bc22dae22e54b8377198
tripdancer0916 <tripdancer0916@gmail.com>
2020-04-13 09:28:24 +0900

    synaptic noiseに対する頑健性を測定。まずは簡単なケースから

---
+++ calc_accurcacy_w_synaptic_noise.py
@@ -0,0 +1,115 @@
+import argparse
+import os
+
+import numpy as np
+import torch
+import yaml
+
+from model import RecurrentNeuralNetwork
+
+
+def romo_signal(delta, N, signal_length, sigma_in):
+    signals = np.zeros([N, 61, 1])
+    freq_range = 4 - abs(delta)
+    for i in range(N):
+        first_signal_timing = 0
+        second_signal_timing = 60 - signal_length
+        first_signal_freq = np.random.rand() * freq_range + max(1, 1 - delta)
+        second_signal_freq = first_signal_freq + delta
+        t = np.arange(0, signal_length / 4, 0.25)
+        phase_shift_1 = np.random.rand() * np.pi
+        first_signal = np.sin(first_signal_freq * t + phase_shift_1) + np.random.normal(0, sigma_in, signal_length)
+        phase_shift_2 = np.random.rand() * np.pi
+        second_signal = np.sin(second_signal_freq * t + phase_shift_2) + np.random.normal(0, sigma_in, signal_length)
+
+        signals[i, first_signal_timing: first_signal_timing + signal_length, 0] = first_signal
+        signals[i, second_signal_timing: second_signal_timing + signal_length, 0] = second_signal
+
+    return signals
+
+
+def main(config_path, sigma_in, signal_length):
+    # hyper-parameter
+    with open(config_path, 'r') as f:
+        cfg = yaml.safe_load(f)
+
+    model_name = os.path.splitext(os.path.basename(config_path))[0]
+
+    os.makedirs('results/', exist_ok=True)
+    save_path = f'results/accuracy_w_synaptic_noise/'
+    os.makedirs(save_path, exist_ok=True)
+
+    print('sigma_syn accuracy')
+    # performanceは1つの学習済みモデルに対してsigma_neu^testを0から0.1まで変えてそれぞれの正解率を記録する。
+    results_acc = np.zeros(11)
+
+    for acc_idx in range(11):
+        # モデルのロード
+        torch.manual_seed(1)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        cfg['MODEL']['SIGMA_NEU'] = 0
+        model = RecurrentNeuralNetwork(n_in=1, n_out=2, n_hid=cfg['MODEL']['SIZE'], device=device,
+                                       alpha_time_scale=0.25, beta_time_scale=cfg['MODEL']['BETA'],
+                                       activation=cfg['MODEL']['ACTIVATION'],
+                                       sigma_neu=cfg['MODEL']['SIGMA_NEU'],
+                                       sigma_syn=cfg['MODEL']['SIGMA_SYN'],
+                                       use_bias=cfg['MODEL']['USE_BIAS'],
+                                       anti_hebbian=cfg['MODEL']['ANTI_HEBB']).to(device)
+
+        model_path = f'trained_model/romo/{model_name}/epoch_{cfg["TRAIN"]["NUM_EPOCH"]}.pth'
+        model.load_state_dict(torch.load(model_path, map_location=device))
+
+        model.eval()
+
+        # add synaptic noise
+        sigma_syn = 0.001 * acc_idx
+        # オリジナルの重み
+        original_w_hh = model.w_hh.weight.data.clone()
+        synaptic_noise = torch.randn((cfg['MODEL']['SIZE'], cfg['MODEL']['SIZE'])) * sigma_syn
+        new_w = original_w_hh + synaptic_noise
+        model.w_hh.weight = torch.nn.Parameter(new_w, requires_grad=False)
+
+        correct = 0
+        num_data = 0
+        # print('delta correct_rate')
+        for delta_idx in range(100):
+            while True:
+                delta = np.random.rand() * 8 - 4
+                if abs(delta) >= 1:
+                    break
+            N = 200
+            output_list = np.zeros(N)
+            input_signal = romo_signal(delta, N, signal_length, sigma_in)
+            input_signal_split = np.split(input_signal, 4)
+            for i in range(4):
+                hidden = torch.zeros(50, model.n_hid)
+                hidden = hidden.to(device)
+                inputs = torch.from_numpy(input_signal_split[i]).float()
+                inputs = inputs.to(device)
+                _, outputs, _, _ = model(inputs, hidden)
+                outputs_np = outputs.cpu().detach().numpy()
+                output_list[i * 50: (i + 1) * 50] = np.argmax(outputs_np[:, -1], axis=1)
+            num_data += 200
+            if delta > 0:
+                ans = 1
+            else:
+                ans = 0
+            correct += (output_list == ans).sum()
+            if delta_idx % 10 == 0:
+                print(delta_idx, delta, (output_list == ans).sum() / 200)
+            # print(f'{delta:.3f}', (output_list == ans).sum() / 200)
+
+        results_acc[acc_idx] = correct / num_data
+        print(cfg['MODEL']['SIGMA_NEU'], correct / num_data)
+
+    np.save(os.path.join(save_path, f'{model_name}.npy'), results_acc)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch RNN training')
+    parser.add_argument('config_path', type=str)
+    parser.add_argument('--sigma_in', type=float, default=0)
+    parser.add_argument('--signal_length', type=int, default=15)
+    args = parser.parse_args()
+    print(args)
+    main(args.config_path, args.sigma_in, args.signal_length)

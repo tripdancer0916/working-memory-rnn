4a0a9f2d2efcf843b50e5c605148e4a77be0a2ce
tripdancer0916 <tripdancer0916@gmail.com>
2020-04-13 10:15:00 +0900

    accuracyを測るのも自動化

---
+++ bash_execute_simple_accuracy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+nohup python -u calc_performance.py cfg/romo_config/1_var_6.cfg &
+
+wait
+
+nohup python -u calc_performance.py cfg/romo_config/6_var_4.cfg &
+nohup python -u calc_performance.py cfg/romo_config/6_var_5.cfg &
+nohup python -u calc_performance.py cfg/romo_config/6_var_6.cfg &
+
+wait
+
+nohup python -u calc_performance.py cfg/romo_config/11_var_2.cfg &
+nohup python -u calc_performance.py cfg/romo_config/11_var_3.cfg &
+
+wait
+
+nohup python -u calc_performance.py cfg/romo_config/11_var_4.cfg &
+nohup python -u calc_performance.py cfg/romo_config/11_var_5.cfg &
+nohup python -u calc_performance.py cfg/romo_config/11_var_6.cfg &
+
+wait
+
+nohup python -u calc_performance.py cfg/romo_config/12_var_2.cfg &
+nohup python -u calc_performance.py cfg/romo_config/12_var_3.cfg &
+
+wait
+
+nohup python -u calc_performance.py cfg/romo_config/12_var_4.cfg &
+nohup python -u calc_performance.py cfg/romo_config/12_var_5.cfg &
+nohup python -u calc_performance.py cfg/romo_config/12_var_6.cfg &
+
+wait
+

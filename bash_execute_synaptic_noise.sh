1ddd3b523aad408c1744694510753e9b677dd618
tripdancer0916 <tripdancer0916@gmail.com>
2020-04-16 19:28:57 +0900

    自動化スクリプト

---
+++ bash_execute_synaptic_noise.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/6_var_1.cfg &
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/6_var_2.cfg &
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/6_var_3.cfg &
+
+wait
+
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/6_var_4.cfg &
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/6_var_5.cfg &
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/6_var_6.cfg &
+
+wait
+
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/11_var_1.cfg &
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/11_var_2.cfg &
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/11_var_3.cfg &
+
+wait
+
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/11_var_4.cfg &
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/11_var_5.cfg &
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/11_var_6.cfg &
+
+wait
+
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/12_var_1.cfg &
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/12_var_2.cfg &
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/12_var_3.cfg &
+
+wait
+
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/12_var_4.cfg &
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/12_var_5.cfg &
+nohup python -u calc_accuracy_w_input_noise.py cfg/romo_config/12_var_6.cfg &
+
+wait
+

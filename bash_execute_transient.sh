eaeab4c289a37963927913ab08243bdae0e73ac6
tripdancer0916 <tripdancer0916@gmail.com>
2020-04-24 07:50:25 +0900

    一気に調べる

--- bash_execute_transient.sh
+++ bash_execute_transient.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+nohup python -u transient_attractor.py cfg/romo_config/1_var_2.cfg &
+nohup python -u transient_attractor.py cfg/romo_config/1_var_3.cfg &
+
+wait
+
+nohup python -u transient_attractor.py cfg/romo_config/1_var_4.cfg &
+nohup python -u transient_attractor.py cfg/romo_config/1_var_5.cfg &
+nohup python -u transient_attractor.py cfg/romo_config/1_var_6.cfg &
+
+wait
+
 nohup python -u transient_attractor.py cfg/romo_config/6_var_1.cfg &
 nohup python -u transient_attractor.py cfg/romo_config/6_var_2.cfg &
 nohup python -u transient_attractor.py cfg/romo_config/6_var_3.cfg &
@@ -12,7 +23,6 @@ nohup python -u transient_attractor.py cfg/romo_config/6_var_6.cfg &
 
 wait
 
-nohup python -u transient_attractor.py cfg/romo_config/11_var_1.cfg &
 nohup python -u transient_attractor.py cfg/romo_config/11_var_2.cfg &
 nohup python -u transient_attractor.py cfg/romo_config/11_var_3.cfg &
 

3d65ba444b2c90dcf280451c41db635c06d2f683
tripdancer0916 <tripdancer0916@gmail.com>
2021-01-05 10:22:38 +0900

    N=512

--- analysis/fixed_point_property_20210104.py
+++ analysis/fixed_point_property_20210104.py
@@ -8,6 +8,7 @@ import torch
 import yaml
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
+from torch.autograd import Variable
 
 sys.path.append('../')
 
@@ -17,6 +18,32 @@ plt.style.use('seaborn-darkgrid')
 import seaborn as sns
 
 sns.set_palette('Dark2')
+
+
+def calc_jacobian(fixed_point, model, device=torch.device('cpu')):
+    const_signal = torch.tensor([0] * 1)
+    const_signal = const_signal.float()
+    fixed_point = torch.unsqueeze(fixed_point, dim=1)
+    fixed_point = Variable(fixed_point).to(device)
+    fixed_point.requires_grad = True
+
+    w_hh = model.w_hh.weight
+    w_hh.requires_grad = False
+    w_hh = w_hh.to(device)
+    activated = torch.tanh(fixed_point)
+    tmp_hidden = torch.unsqueeze(model.w_in(const_signal), dim=1) + w_hh @ activated
+
+    activated = tmp_hidden - fixed_point
+
+    jacobian = torch.zeros(model.n_hid, model.n_hid)
+    for i in range(model.n_hid):
+        output = torch.zeros(model.n_hid, 1).to(device)
+        output[i] = 1.
+        jacobian[:, i:i + 1] = torch.autograd.grad(activated, fixed_point, grad_outputs=output, retain_graph=True)[0]
+
+    jacobian = jacobian.cpu().numpy().T
+
+    return jacobian
 
 
 def calc_speed(hidden_activated, model):
@@ -85,6 +112,38 @@ def main(config_path):
     )
 
     plt.savefig(f'results/{model_name}/fixed_points.png', dpi=300)
+
+    fixed_point_list = []
+    for i in range(50):
+        for j in range(4):
+            fixed_point = np.loadtxt(f'../fixed_points/freq/{model_name}/fixed_point_{i}_{j}.txt')
+            if calc_speed(torch.from_numpy(fixed_point).float(), model) < 0.02:
+                fixed_point_list.append(fixed_point)
+            # count += 1
+
+    pc_fixed_point_list = pca.transform(fixed_point_list)
+    plt.figure(constrained_layout=True)
+    plt.scatter(
+        pc_fixed_point_list[:, 0],
+        pc_fixed_point_list[:, 1],
+        s=5,
+    )
+    plt.xlabel('PC1', fontsize=16)
+    plt.ylabel('PC2', fontsize=16)
+    plt.title(r'Only speed $\leq 0.02$', fontsize=14)
+
+    plt.savefig(f'results/{model_name}/fixed_points_2dim.png', dpi=300)
+
+    jacobian = calc_jacobian(torch.from_numpy(fixed_point_list[0]).float(), model)
+    w, v = np.linalg.eig(jacobian)
+    plt.figure(constrained_layout=True)
+    plt.scatter(
+        w.real,
+        w.imag,
+    )
+    plt.title(f'Eigenvalue Distribution, {np.max(w.real)}', fontsize=16)
+    plt.savefig(f'results/{model_name}/fixed_point_eig.png', dpi=300)
+    print(w[w.real > 0])
 
 
 if __name__ == '__main__':
